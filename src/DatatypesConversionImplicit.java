fe7d22f382e926b6166079c5a32de60557a4c766
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 15:11:57 -0800

    Initial commit

---
+++ src/DatatypesConversionImplicit.java
@@ -0,0 +1,10 @@
+public class DatatypesConversionImplicit {
+    public static void main(String[] args) {
+        int z = 200;
+        System.out.println("int representation: "+z);
+        long y = z;
+        System.out.println("long representation: "+y);
+        float x = y;
+        System.out.println("float representation: "+x);
+    }
+}
